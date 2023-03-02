0bb06d9cde7a7683bed83aad64d38c14887bb603
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-03 01:16:51 +0600

    initial card design done

--- script/app.js
+++ script/app.js
@@ -5,7 +5,40 @@ const loadData = async () => {
     const data = await res.json();
     displayData(data.data.tools);
 }
-const displayData = items => {
-    console.log(items);
+const displayData = aiItems => {
+    const aiContainer = document.getElementById('ai-container');
+    aiItems = aiItems.slice(0, 6);
+
+
+    aiItems.forEach(aiItem => {
+        const aiDiv = document.createElement('div');
+        aiDiv.classList.add('col');
+        aiDiv.innerHTML = `
+        <div class="card p-3 h-100">
+            <img src="${aiItem.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                <ol>
+                    <li>Natural language processing</li>
+                    <li>Natural language processing</li>
+                    <li>Natural language processing</li>
+                </ol>
+                <hr>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                    <h5 class="card-title">${aiItem.name}</h5>
+                    <span>${aiItem.published_in}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-arrow-right-long"></i>
+                    </div>
+                </div>
+            </div>
+      </div>
+        `
+
+        aiContainer.appendChild(aiDiv);
+
+    });
 }
 loadData();
