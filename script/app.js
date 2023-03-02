6448916ff0113a784a6ae9920687d7b856b19732
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-02 23:13:36 +0600

    fixe card added

--- script/app.js
+++ script/app.js
@@ -1,1 +1,11 @@
-console.log('script file connected');
+
+const loadData = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data.tools);
+}
+const displayData = items => {
+    console.log(items);
+}
+loadData();
