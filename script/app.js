afceaea7e5c4fabe0ca6c9e801b21d0bb03bbdb8
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-03 22:17:56 +0600

    show more button working

--- script/app.js
+++ script/app.js
@@ -1,14 +1,17 @@
 
-const loadData = async () => {
+const loadData = async (primaryData) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayData(data.data.tools);
+    displayData(data.data.tools, primaryData);
 }
-const displayData = aiItems => {
+const displayData = (aiItems, primaryData) => {
     const aiContainer = document.getElementById('ai-container');
-       
-    aiItems = aiItems.slice(0, 6);
+
+    if (primaryData === 6) {
+
+        aiItems = aiItems.slice(0, 6);
+    }
 
     aiItems.forEach(aiItem => {
         const aiDiv = document.createElement('div');
@@ -18,12 +21,11 @@ const displayData = aiItems => {
             <img src="${aiItem.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
-                <ol>
-                    <li>Natural language processing</li>
-                    <li>Natural language processing</li>
-                    <li>Natural language processing</li>
+
+                <ol id="${50+aiItem.id}">
                 </ol>
                 <hr>
+
                 <div class="d-flex justify-content-between align-items-center">
                     <div>
                     <h5 class="card-title">${aiItem.name}</h5>
@@ -39,12 +41,23 @@ const displayData = aiItems => {
 
         aiContainer.appendChild(aiDiv);
 
+// add feature on ol tag and append
+        aiItem.features.forEach(feature => {
+            // crate li tag
+            const li = document.createElement('li');
+            li.innerText = feature;
+
+            // get ol tag and append 
+            const featuresContainer = document.getElementById(`${50+aiItem.id}`);
+            featuresContainer.appendChild(li);
+        });
+        
     });
     
-    
-    
-    
-    
+
+
+
+
     // stop loading spinner ;
     const loadingSpinner = document.getElementById('spinner');
     loadingSpinner.classList.add('d-none');
@@ -62,10 +75,32 @@ const displayData = aiItems => {
 
 // show button eventlistener
 
-document.getElementById('see-more-btn').addEventListener('click', function(){
-   
+document.getElementById('see-more-btn').addEventListener('click', function () {
+    // clear the previous html
+    const previousHtml = document.getElementById('ai-container');
+    previousHtml.innerHTML = '';
+
+
+       // start loading spinner ;
+       const loadingSpinner = document.getElementById('spinner');
+       loadingSpinner.classList.remove('d-none');
+       // view short by date button;
+       const shortByDateBtn = document.getElementById('short-by-date-btn');
+       shortByDateBtn.classList.add('d-none');
+       // view see more button;
+       const seeMoreBtn = document.getElementById('see-more-btn');
+       seeMoreBtn.classList.add('d-none');
+
+
+
+    // call the data without parameter
+    loadData()
+
 })
 
+
+
+// ai details in modal 
 const loadAiDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     const res = await fetch(url);
@@ -82,12 +117,12 @@ const displayAiDetails = details => {
     // modal second card;
     const secondCardContainer = document.getElementById('modal-second-card');
     secondCardContainer.innerHTML = `
-    <img src="${details.image_link[0] ? details.image_link[0]:details.image_link[1]}" class="card-img-top" alt="...">
-    <h5>${details.input_output_examples[0] ? details.input_output_examples[0].input:"Can you give any example?"} </h5>
-    <p>${details.input_output_examples[0] ? details.input_output_examples[0].output:"No! Not Yet! Take a break!!!"} </p>
+    <img src="${details.image_link[0] ? details.image_link[0] : details.image_link[1]}" class="card-img-top" alt="...">
+    <h5>${details.input_output_examples[0] ? details.input_output_examples[0].input : "Can you give any example?"} </h5>
+    <p>${details.input_output_examples[0] ? details.input_output_examples[0].output : "No! Not Yet! Take a break!!!"} </p>
     `
-    
-    console.log(details);
+
+
 }
 
-loadData();
+loadData(6);
